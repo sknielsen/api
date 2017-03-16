9df4eb99fe7bfa29c5f3d9e6ee926aa868320ab2
Samantha Nielsen <samanthan@concur.com>
2017-03-16 12:05:00 -0700

    Use jinja to call in html from templates

--- server.py
+++ server.py
@@ -1,66 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return '<html><body><h1>I am the landing page</h1></body></html>'
-
-@app.route('/warmercolder')
 def warmer_or_colder():
-    return """
-<html>
-    <head>
-        <title>Warmer or Colder</title>
-        <link rel="stylesheet" href="/static/warmer.css">
-    </head>
-    <body>
-        <h1>Warmer or Colder</h1>
-        <br>
-        <p>Will today be warmer or colder than yesterday?</p>
-        <form>
-            Zip Code<br>
-            <input type="text" name="zip code">
-            <br>
-            <input type="submit" value="Submit">
-        </form>
-     </body>
-</html>"""
+    return render_template('warmer.html')
 
-@app.route('/warmer')
-def warmer():
-    return"""
-<html>
-    <head>
-    <link rel="stylesheet" href="/static/warmer.css">
-    </head>
-    <body>
-        <h1>Warmer or Colder</h1>
-        <br>
-        <p>Will today be warmer or colder than yesterday?</p>
-        <br><br>
-        <p>It will be warmer than yesterday!</>
-        <br>
-        <img src="/static/sun.png" alt="Sun">
-"""
-
-@app.route('/colder')
-def colder():
-    return"""
-<html>
-    <head>
-    <link rel="stylesheet" href="/static/warmer.css">
-    </head>
-    <body>
-        <h1>Warmer or Colder</h1>
-        <br>
-        <p>Will today be warmer or colder than yesterday?</p>
-        <br><br>
-        <p>It will be colder than yesterday!</>
-        <br>
-        <br>
-        <img src="/static/snowflake.png" alt="Snowflake" style="width:200px;height:200px">
-"""
+@app.route('/answer')
+def answer():
+    answer = 'warmer'
+    image = '/static/sun.png'
+    alt = 'sun'
+    return render_template('answer.html', answer=answer, image=image, alt=alt)
 
 if __name__ == "__main__":
       app.run(debug=True)
--- weather.py
+++ weather.py
@@ -1,10 +1,15 @@
 import urllib2
 import json
+from pprint import pprint
+
 yesterday = urllib2.urlopen('http://api.wunderground.com/api/856474bb0ccfb13e/yesterday/q/CA/San_Francisco.json')
 today = urllib2.urlopen('http://api.wunderground.com/api/856474bb0ccfb13e/forecast/q/CA/San_Francisco.json')
 yesterday_json_string = yesterday.read()
 today_json_string = today.read()
 yesterday_parsed_json = json.loads(yesterday_json_string)
+pprint(yesterday_parsed_json)
+y_temp = yesterday_parsed_json['history']['dailysummary'][0]['maxtempi']
+print y_temp
 today_parsed_json = json.loads(today_json_string)
 today.close()
 yesterday.close()
